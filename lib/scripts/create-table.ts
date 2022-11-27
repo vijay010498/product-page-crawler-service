40b2930467439a4c6874abae114acc8d70d6092e
vijay010498 <svijayr007@gmail.com>
2022-11-27 21:34:54 +0530

    updated FK constraint

--- lib/scripts/create-table.ts
+++ lib/scripts/create-table.ts
@@ -7,15 +7,12 @@ const createTable = async (): Promise<Boolean> => {
       await postgresClient.query(
         `CREATE TABLE IF NOT EXISTS ${postgresConstants.crawledResultsTable}
                  (
-                    job_id varchar(50),
+                    job_id varchar(50) FOREIGN KEY (job_id) REFERENCES ${postgresConstants.crawlQueueTable}(job_id),
                     title varchar(150) NOT NULL,
                     brand varchar(100) NOT NULL,
                     image_url varchar(255) NOT NULL,
                     result_id varchar(50) PRIMARY KEY,
-                    created_at timestamp NOT NULL DEFAULT NOW(),
-                    CONSTRAINT fk_crawlQueue
-                        FOREIGN KEY (job_id)
-                        REFERENCES ${postgresConstants.crawlQueueTable}(job_id)
+                    created_at timestamp NOT NULL DEFAULT NOW()
                  )`
       );
       resolve(true);
